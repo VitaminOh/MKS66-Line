ffea92764ee010d6cd6f10e72a0e453b96fbba0b
VitaminOh <dlin01@stuy.edu>
2020-02-10 11:27:51 -0500

    vertical and horizontal lines now work

--- draw.py
+++ draw.py
@@ -2,7 +2,7 @@ from display import *
 
 def octant_number(x0, y0, x1, y1):
     slope = (y1 - y0) / (x1 - x0)
-    if slope > 0 and slope <= 1:
+    if slope >= 0 and slope <= 1:
         return 1
     elif slope > 1:
         return 2
@@ -12,68 +12,77 @@ def octant_number(x0, y0, x1, y1):
         return 7
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    # Make sure to check for horizontal or vertical lines here
-    # Octant I
-    octant = octant_number(x0, y0, x1, y1)
-    A = y1 - y0
-    B = -1 * (x1 - x0)
-    if octant == 1:
-        if y0 > y1 and x0 > x1:
-            draw_line(x1, y1, x0, y0, screen, color)
-        else:
-            for i in range(x1 - x0):
-                x = x0
-                y = y0
-                d = 2 * A + B
-                while x <= x1:
-                    plot(screen, color, x, y)
-                    if d > 0:
+    # Vertical lines
+    try:
+        octant = octant_number(x0, y0, x1, y1)
+        A = y1 - y0
+        B = -1 * (x1 - x0)
+        if octant == 1:
+            if y0 > y1 and x0 > x1:
+                draw_line(x1, y1, x0, y0, screen, color)
+            else:
+                for i in range(x1 - x0):
+                    x = x0
+                    y = y0
+                    d = 2 * A + B
+                    while x <= x1:
+                        plot(screen, color, x, y)
+                        if d > 0:
+                            y += 1
+                            d += 2 * B
+                        x += 1
+                        d += 2 * A
+        if octant == 2:
+            if y0 > y1 and x0 > x1:
+                draw_line(x1, y1, x0, y0, screen, color)
+            else:
+                for i in range(x1 - x0):
+                    x = x0
+                    y = y0
+                    d = A + 2 * B
+                    while y <= y1:
+                        plot(screen, color, x, y)
+                        if d < 0:
+                            x += 1
+                            d += 2 * A
                         y += 1
                         d += 2 * B
-                    x += 1
-                    d += 2 * A
-    if octant == 2:
-        if y0 > y1 and x0 > x1:
-            draw_line(x1, y1, x0, y0, screen, color)
-        else:
-            for i in range(x1 - x0):
-                x = x0
-                y = y0
-                d = A + 2 * B
-                while y <= y1:
-                    plot(screen, color, x, y)
-                    if d < 0:
+        if octant == 8:
+            if y0 < y1 and x0 > x1:
+                draw_line(x1, y1, x0, y0, screen, color)
+            else:
+                for i in range(x1 - x0):
+                    x = x0
+                    y = y0
+                    d = 2 * A - B
+                    while x <= x1:
+                        plot(screen, color, x, y)
+                        if d < 0:
+                            y -= 1
+                            d += -2 * B
                         x += 1
                         d += 2 * A
-                    y += 1
-                    d += 2 * B
-    if octant == 8:
-        if y0 < y1 and x0 > x1:
-            draw_line(x1, y1, x0, y0, screen, color)
-        else:
-            for i in range(x1 - x0):
-                x = x0
-                y = y0
-                d = 2 * A - B
-                while x <= x1:
-                    plot(screen, color, x, y)
-                    if d < 0:
+        if octant == 7:
+            if y0 < y1 and x0 > x1:
+                draw_line(x1, y1, x0, y0, screen, color)
+            else:
+                for i in range(x1 - x0):
+                    x = x0
+                    y = y0
+                    d = A + -2 * B
+                    while y >= y1:
+                        plot(screen, color, x, y)
+                        if d > 0:
+                            x += 1
+                            d += 2 * A
                         y -= 1
                         d += -2 * B
-                    x += 1
-                    d += 2 * A
-    if octant == 7:
-        if y0 < y1 and x0 > x1:
+    except ZeroDivisionError as e:
+        if y0 > y1:
             draw_line(x1, y1, x0, y0, screen, color)
         else:
-            for i in range(x1 - x0):
-                x = x0
+            for i in range(y1 - y0):
                 y = y0
-                d = A + -2 * B
-                while y >= y1:
-                    plot(screen, color, x, y)
-                    if d > 0:
-                        x += 1
-                        d += 2 * A
-                    y -= 1
-                    d += -2 * B
+                while y <= y1:
+                    plot(screen, color, x0, y)
+                    y += 1
